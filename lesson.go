b67fb80ae61e779ec348fe6f0f25608e8903a979
小林和希 <kobayashi.kazuki74@gmail.com>
2018-11-18 16:48:23 +0900

    hello, world

--- lesson.go
+++ lesson.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func init () {
+	// main関数より先に呼ばれる
+	fmt.Println("init")
+}
+
+func bazz () {
+	//定義しただけでは実行されない
+	fmt.Println("Bazz")
+}
+
 func main () {
-	fmt.Println("Hello, World!!!")
+	//実行が必要
+	bazz()
+	//大文字から始まる関数はPublic
+	fmt.Println("Hello, World!", "Hello, Go!")
+	//コンマで繋げられる
 }
